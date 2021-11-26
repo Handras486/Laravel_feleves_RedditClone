1c6789b905bf08cdaa24e8d9fab3d05eacfa2946
András Hajdú <hajduandras486@gmail.com>
2021-11-26 16:27:35 +0100

    Dependency management with Laravel Mix WIP

--- RedditClone/resources/views/layouts/main.blade.php
+++ RedditClone/resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-    <link href="/css/blog.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 
     <meta name="theme-color" content="#7952b3">
 
@@ -51,6 +51,7 @@
         </p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="/js/reply.js"></script>
     @stack('scripts')
 </body>
 </html>
