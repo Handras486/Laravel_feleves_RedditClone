7d3f336c7343d4b6b42f6e7198fcfcad4a046de7
András Hajdú <hajduandras486@gmail.com>
2021-12-09 02:40:23 +0100

    Improved Post show visually

--- RedditClone/resources/views/posts/show.blade.php
+++ RedditClone/resources/views/posts/show.blade.php
@@ -1,26 +1,47 @@
 @extends('layouts.main')
 
 @section('content')
-<form action="{{ route('post.vote', $post) }}" method="POST">
-    @csrf
-    <input type="submit" name="type" value="True" />     
-</form>
-<a>{{ $post->score}}</a>
-<form action="{{ route('post.vote', $post) }}" method="POST">
-    @csrf
-    <input type="submit" name="type" value="False" />     
-</form>
-<h1 class="display-1">{{ $post->title }}</h1>
-<p> {{ $post->score }}|{{ $post->author->name }} | {{ $post->subreddit->name }} | {{ $post->updated_at->diffForHumans() }}</p>
-<a href="{{ route('post.edit', $post) }}">edit</a>
-<div class="">
-    <form action="{{ route('post.delete', $post) }}" method="POST">
-        @csrf
-        <input type="submit" value="delete"/>
-    </form>
-</div>
-<div>
-    {!! $post->content !!}
+<div class="flex-container">
+    <div class="d-flex flex-row align-items-center"> 
+        <div class="d-flex flex-column m-3 align-items-center">
+            <form action="{{ route('post.vote', $post) }}" method="POST">
+                @csrf
+                <input type="submit" name="type" value="True" />     
+            </form>
+            <a>{{ $post->score}}</a>
+            <form action="{{ route('post.vote', $post) }}" method="POST">
+                @csrf
+                <input type="submit" name="type" value="False" />     
+            </form>
+        </div>
+        <div class="m-3">
+            <img class="" src="{{ $post->cover_image }}" width="100" height="100" alt="{{ $post->title }}">
+        </div>
+        <div class="d-flex flex-column m-3 small">
+            <a href="{{ route('post.details', $post) }}"><h1>{{ $post->title }}</h1></a>
+            <div class="">
+                <a>submitted {{ $post->created_at->diffForHumans() }} by</a>
+                <a href="#">{{ $post->author->name }}</a>
+                <a>to</a>
+                <a href="{{ route('subreddit.details', $post->subreddit) }}">{{ $post->subreddit->name }}</a>
+            </div>
+            <div class="bg-light">
+                {!! $post->content !!}
+            </div>
+            <div class="d-flex flex-row">
+                <div class="px-2">
+                    <a href="{{ route('post.details', $post) }}">{{ $post->comments()->count()}}+ comments</a>
+                    <a href="{{ route('post.edit', $post) }}">edit</a>
+                </div>
+                <div class="">
+                    <form action="{{ route('post.delete', $post) }}" method="POST">
+                        @csrf
+                        <input type="submit" value="delete"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <div class="row">
     <div class="col-md-8 col-lg-6 mx-auto">
